2ffe97cb6509804d1029c423f690936c1356b81d
yeohl <89959016+yeohl@users.noreply.github.com>
2021-12-15 22:15:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,17 +7,12 @@ import os
 path = 'face_images'
 images = []
 KnownNames = []
-KnownFaces = os.listdir(path)
-print(KnownFaces)
-for cl in KnownFaces:
+Knownface = []
+dir = os.listdir(path)
+print(dir)
+
+for cl in dir:
     curImg = cv2.imread(f'{path}/{cl}')
     images.append(curImg)
     KnownNames.append(os.path.splitext(cl)[0])
-print(KnownNames)
-
-imgKim = face_recognition.load_image_file('face_images/Kim.jpg')
-imgKim = cv2.cvtColor(imgKim.cv2.COLOR_BGR2RGB)
-imgHee = face_recognition.load_image_file('face_images/Heejin.jpg')
-imgHee = cv2.cvtColor(imgHee.cv2.COLOR_BGR2RGB)
-imgJin = face_recognition.load_image_file('face_images/Jin.jpg')
-imgJin = cv2.cvtColor(imgJin.cv2.COLOR_BGR2RGB)
+    Knownface.append(curImg)
